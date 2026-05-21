c1ece0929e489fffaf5d7f971310596c67b380b2
Anjalipradeepkumar <anjali@redeftec.com>
2026-05-21 18:08:02 +0530

    Fix CORS for production

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:4200',
+    process.env.CLIENT_URL
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/employees', employeeRoutes);
